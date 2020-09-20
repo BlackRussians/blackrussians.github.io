c567d80de9e3be406d47d70cca985dde097aaa61
Soobin Choi <jkz@naver.com>
2020-09-20 14:24:28 +0900

    Updates

---
+++ precache-manifest.8d280a51c1e0208750b0e3a005249ad8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd3adae635a3a5a5587185f2bdb008a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78334742c1d1964c794f",
+    "url": "/static/css/2.783d4bc2.chunk.css"
+  },
+  {
+    "revision": "02d065cf7882b87f25b5",
+    "url": "/static/css/main.66ce85f5.chunk.css"
+  },
+  {
+    "revision": "78334742c1d1964c794f",
+    "url": "/static/js/2.fad6b2d9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.fad6b2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d065cf7882b87f25b5",
+    "url": "/static/js/main.5e70f226.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "c7c99d7b11c6855578b16261e66cf9a2",
+    "url": "/static/media/quiz.c7c99d7b.jpeg"
+  }
+]);
